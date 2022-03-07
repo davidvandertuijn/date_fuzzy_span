fd6d5351851062bdbe3867ac1f93a549411983ec
David van der Tuijn <david@digitalimpact.nl>
2022-03-07 20:40:03 +0100

    Code Optimization

--- src/date_fuzzy_span.php
+++ src/date_fuzzy_span.php
@@ -3,66 +3,66 @@
 /**
  * Date Fuzzy Span.
  *
- * @param int $iTimestamp
+ * @param int $timestamp
  *
  * @return string
  */
-function date_fuzzy_span(int $iTimestamp): string
+function date_fuzzy_span(int $timestamp): string
 {
-    $iYear = 31556926;
-    $iMonth = 2629744;
-    $iWeek = 604800;
-    $iDay = 86400;
-    $iHour = 3600;
-    $iMinute = 60;
+    $year = 31556926;
+    $month = 2629744;
+    $week = 604800;
+    $day = 86400;
+    $hour = 3600;
+    $minute = 60;
 
-    $iOffset = abs(time() - $iTimestamp);
+    $offset = abs(time() - $timestamp);
 
-    if ($iOffset <= $iMinute) {
-        $sSpan = 'moments';
-    } elseif ($iOffset < ($iMinute * 20)) {
-        $sSpan = 'a few minutes';
-    } elseif ($iOffset < $iHour) {
-        $sSpan = 'less than an hour';
-    } elseif ($iOffset < ($iHour * 4)) {
-        $sSpan = 'a couple of hours';
-    } elseif ($iOffset < $iDay) {
-        $sSpan = 'less than a day';
-    } elseif ($iOffset < ($iDay * 2)) {
-        $sSpan = 'about a day';
-    } elseif ($iOffset < ($iDay * 4)) {
-        $sSpan = 'a couple of days';
-    } elseif ($iOffset < $iWeek) {
-        $sSpan = 'less than a week';
-    } elseif ($iOffset < ($iWeek * 2)) {
-        $sSpan = 'about a week';
-    } elseif ($iOffset < $iMonth) {
-        $sSpan = 'less than a month';
-    } elseif ($iOffset < ($iMonth * 2)) {
-        $sSpan = 'about a month';
-    } elseif ($iOffset < ($iMonth * 4)) {
-        $sSpan = 'a couple of months';
-    } elseif ($iOffset < $iYear) {
-        $sSpan = 'less than a year';
-    } elseif ($iOffset < ($iYear * 2)) {
-        $sSpan = 'about a year';
-    } elseif ($iOffset < ($iYear * 4)) {
-        $sSpan = 'a couple of years';
-    } elseif ($iOffset < ($iYear * 8)) {
-        $sSpan = 'a few years';
-    } elseif ($iOffset < ($iYear * 12)) {
-        $sSpan = 'about a decade';
-    } elseif ($iOffset < ($iYear * 24)) {
-        $sSpan = 'a couple of decades';
-    } elseif ($iOffset < ($iYear * 64)) {
-        $sSpan = 'several decades';
+    if ($offset <= $minute) {
+        $span = 'moments';
+    } elseif ($offset < ($minute * 20)) {
+        $span = 'a few minutes';
+    } elseif ($offset < $hour) {
+        $span = 'less than an hour';
+    } elseif ($offset < ($hour * 4)) {
+        $span = 'a couple of hours';
+    } elseif ($offset < $day) {
+        $span = 'less than a day';
+    } elseif ($offset < ($day * 2)) {
+        $span = 'about a day';
+    } elseif ($offset < ($day * 4)) {
+        $span = 'a couple of days';
+    } elseif ($offset < $week) {
+        $span = 'less than a week';
+    } elseif ($offset < ($week * 2)) {
+        $span = 'about a week';
+    } elseif ($offset < $month) {
+        $span = 'less than a month';
+    } elseif ($offset < ($month * 2)) {
+        $span = 'about a month';
+    } elseif ($offset < ($month * 4)) {
+        $span = 'a couple of months';
+    } elseif ($offset < $year) {
+        $span = 'less than a year';
+    } elseif ($offset < ($year * 2)) {
+        $span = 'about a year';
+    } elseif ($offset < ($year * 4)) {
+        $span = 'a couple of years';
+    } elseif ($offset < ($year * 8)) {
+        $span = 'a few years';
+    } elseif ($offset < ($year * 12)) {
+        $span = 'about a decade';
+    } elseif ($offset < ($year * 24)) {
+        $span = 'a couple of decades';
+    } elseif ($offset < ($year * 64)) {
+        $span = 'several decades';
     } else {
-        $sSpan = 'a long time';
+        $span = 'a long time';
     }
 
-    if ($iTimestamp <= time()) {
-        return $sSpan.' ago';
+    if ($timestamp <= time()) {
+        return $span.' ago';
     } else {
-        return 'in '.$sSpan;
+        return 'in '.$span;
     }
 }
